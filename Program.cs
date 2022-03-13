2a1165cc58356e7d84fd4f96e7b972398421190c
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 19:12:00 +0530

    Calulate the Length of line

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCP
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            UC1 uc1 = new UC1();
+            uc1.Problem1();
+        }
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCP
+{
+	public class UC1
+	{
+		public void Problem1()
+		{
+			int x1, x2, y1, y2;
+
+			Console.WriteLine("Enter X1 Value:");
+			x1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter X2 Value:");
+			y1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter Y1 Value:");
+			x2 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter Y2 Value:");
+			y2 = Convert.ToInt32(Console.ReadLine());
+
+			double Z = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+			Console.WriteLine("Length of Line is : {0}", Z);
+		}
+	}
+}
